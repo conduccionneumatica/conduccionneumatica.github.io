277f766c04c4cab12b0ca349a5cd3a66bf443353
Thiago Hersan <thiago.hersan+github@gmail.com>
2021-01-11 00:18:06 -0500

    auto resize videos for max size

--- js/content.js
+++ js/content.js
@@ -1,10 +1,11 @@
-let mFrameTop, mFrameMiddle, mFrameBottom, mPageWrapper;
+let mFrameTop, mFrameMiddle, mFrameBottom, mPageWrapper, mContentText;
 
 function setElements() {
   mFrameTop = document.getElementById('my-frame-top');
   mFrameMiddle = document.getElementById('my-frame-middle');
   mFrameBottom = document.getElementById('my-frame-bottom');
   mPageWrapper = document.getElementById('my-page-wrapper');
+  mContentText = document.getElementById('my-content-text');
 }
 
 function setPaddings() {
@@ -14,14 +15,32 @@ function setPaddings() {
   mPageWrapper.style.paddingBottom = `${bottomHeight}px`;
 }
 
+function sizeVideos() {
+  const maxW = mContentText.offsetWidth;
+  const maxH = mFrameMiddle.offsetHeight;
+
+  for(const v of document.getElementsByClassName('video-wrapper-sizer')) {
+    if((maxW / maxH) > (16 / 9)) {
+      v.style.height = `${maxH}px`;
+      v.style.width = `${maxH * 16 / 9}px`;
+    } else {
+      v.style.width = `${maxW}px`;
+      v.style.height = `${maxW * 9 / 16}px`;
+    }
+  }
+}
+
 window.addEventListener('load', () => {
   setElements();
   setPaddings();
+  sizeVideos();
 });
 
 window.addEventListener('resize', () => {
   setPaddings();
+  sizeVideos();
 });
 
 setElements();
 setPaddings();
+sizeVideos();
